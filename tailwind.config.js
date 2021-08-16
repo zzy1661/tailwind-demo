4cc4e689028c981f88460534c05b2aa31c8e2677
周振宇 <zhouzhenyu@xforceplus.com>
2021-08-16 13:46:47 +0800

    fix:颜色修改

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,5 @@
 const plugin = require('tailwindcss/plugin')
 
-// $spaces: 0, 8, 10, 12, 16, 20, 32, 40, 56, 64, 96, 128, 240, 480;
-// $fontSize: 0, 24, 28, 32, 36, 40, 48, 56, 64;
-// $widths: 44, 240, 400, 480;
-// $heights: 200, 300;
-// $boxSize: 80, 100, 120, 200;
-
-// 基本填充颜色列表 //浅黄#
-// $color-primary: #5965ba;
-// $color-secondary: #999;
-// $color-success: #1bb73a;
-// $color-danger: #e83930;
-// $color-warning: #fa8c16;
-// $color-info: #FFE56C;
-// $color-dark: #333;
-// 带场景的填充颜色
-// 选中
-// $color-primary-active:#5965ba;
-// $color-secondary-active: #999;
-// $color-success-active:#1bb73a;
-// $color-danger-active: #e83930;
-// $color-warning-active: #fa8c16;
-// $color-info-active:#FFE56C;
-// $color-dark-active: #333;
-// 不可操作,需要透明度TODO
-// $color-primary-disable:#5965ba;
-// $color-secondary-disable:#999
-// $color-success-disable:#1bb73a;
-// $color-danger-disable: #e83930;
-// $color-warning-disable: #fa8c16;
-// $color-info-disable:#FFE56C;
-// $color-dark-disable: #333;
-
 
 
 /**
@@ -62,7 +30,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      // 为了复用colors
+      // 为了复用colors，放在extend中，会覆盖colors中冲突的配置
       textColor: {
         // 基本文字颜色列表 文字的secondary和color的不一致，需要确认设计稿，缺失选中和disable
         "basic": {
@@ -80,11 +48,6 @@ module.exports = {
           active: "#fff",
           disable: '"#fff"'
         },
-        "tinge": {
-          DEFAULT: "#b476c0",
-          active: "#b476c0",
-          disable: '"#b476c0"'
-        },
         "secondary": {
           DEFAULT: "#666666",
           active: "#666666",
@@ -95,50 +58,78 @@ module.exports = {
           active: "#000",
           disable: '"#000"'
         },
-        // "primary": "#5965ba",
-        // "success": "#1bb73a",
-        // "danger": "#e83930",
-        // "warning": "#fa8c16", //#FB9716
-        // "info": "#ffe56c",
       },
     },
+    // 背景、文字等颜色
     colors: {
+      golden: '#DAA341',
+      gray: '#F0F0F0',
       // 基本填充颜色列表,缺失选中/禁用的效果
       "primary": {
         DEFAULT: "#5965ba",
         active: "#5965ba",
-        disable: "#5965ba",
+        disable: "rgba(89, 101, 186, 0.1)",
       },
       "secondary": {
         DEFAULT: "#999",
         active: "#999",
-        disable: "#999",
+        disable: "rgba(153, 153, 153, 0.1)",
       },
       "success": {
         DEFAULT: "#1bb73a",
         active: "#1bb73a",
-        disable: "#1bb73a",
+        disable: "rgba(27, 183, 58, 0.1)",
       },
       "danger": {
-        DEFAULT: "#e83930",
-        active: "#e83930",
-        disable: "#e83930",
+        DEFAULT: "#FA3A1A",
+        active: "#FA3A1A",
+        disable: "rgba(250, 58, 26, 0.1)",
       },
       "warning": {
         DEFAULT: "#fa8c16",
         active: "#fa8c16",
-        disable: "#fa8c16",
+        disable: "rgba(250, 140, 22, 0.1)",
       },
       "info": {
-        DEFAULT: "#FFE56C",
-        active: "#FFE56C",
-        disable: "#FFE56C",
+        DEFAULT: "#b476c0",
+        active: "#b476c0",
+        disable: 'rgba(180, 118, 192, 0.1)'
+      },
+      "required": {// 必填后面的星号
+        DEFAULT: '#E83930',
+        active: '#E83930',
+        disable: 'rgba(232, 57, 48, 0.1)'
+      },
+      "light": {  //高亮，类似warning的颜色
+        DEFAULT: '#FB9716',
+        active: '#FB9716',
+        disable: 'rgba(251, 151, 22, 0.1)'
       },
       "dark": {
         DEFAULT: "#333",
         active: "#333",
-        disable: "#333",
+        disable: "rgba(51, 51, 51, 0.1)",
       },
+      "white": {
+        DEFAULT: '#FFF',
+        active: '#fff',
+        disable: 'rgba(255,255,255,0.3)'
+      },
+      "bottom": {// 底色，通常用作背景
+        DEFAULT: '#F8F8FA',
+        active: '#F8F8FA',
+        disable: "rgba(248, 248, 250, 0.1)"
+      },
+      "guide": { //诱导点击的颜色，如展开、更多，类似primary
+        DEFAULT: '#2E42C7',
+        active: '#2E42C7',
+        disable: "rgba(46, 66, 199, 0.1)"
+      },
+      "tip": { //类似成功的积极提示
+        DEFAULT: '#14C437',
+        active: '#14C437',
+        disable: "rgba(20, 196, 55, 0.1)"
+      }
     },
 
     spacing: {
