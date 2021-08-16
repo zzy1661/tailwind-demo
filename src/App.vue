d648761de668c5fdd90306f875259d195922d4ab
周振宇 <zhouzhenyu@xforceplus.com>
2021-08-16 10:45:48 +0800

    feat: add base

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="box-80 border"></div>
+    <div class="text-sm">text sm</div>
+    <div class="text-base">text-base</div>
+    <div class="text-lg">text-lg</div>
+    <div class="text-lg leading-loose">text-lg leading-loose aaaa 啊一三丫</div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -18,15 +23,19 @@ export default {
 
 <style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  @apply flex flex-col;
+
+  @apply flex flex-col text-base;
   img {
     @apply w-400;
   }
+}
+
+.btn {
+  @apply w-400 h-64 border-0 bg-primary;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
 
 // $spaces: 0, 8, 10, 12, 16, 20, 32, 40, 56, 64, 96, 128, 240, 480;
 // $fontSize: 0, 24, 28, 32, 36, 40, 48, 56, 64;
@@ -32,34 +32,27 @@
 // $color-info-disable:#FFE56C;
 // $color-dark-disable: #333;
 
-// 基本文字颜色列表 //近蓝B476C0
-// $text-basic: #333333;
-// $text-muted: #999999;
-// $text-light: #fff;
-// $text-primary: #5965ba;
-// $text-tinge: #b476c0;
-// $text-secondary: #666666;
-// $text-success: #1bb73a;
-// $text-danger: #e83930;
-// $text-warning: #fa8c16; //#FB9716
-// $text-info: #ffe56c;
-// $text-dark: #000;
+
 
 /**
  * 1. 先处理spacing
  * 2. 处理color
  * 3. 自定义box等
  * 4. 自定义组件
+ * 5. 考虑dark模式
+ * 6. 全局的默认样式
+ * 7. 验证样式是否缺漏
  * 
  * 
  * 
  * 
  */
+// 一下数字单位都是750设计稿的px
 const spacings = [0, 8, 10, 12, 16, 20, 32, 40, 56, 64, 96, 128, 240, 480];
-const fontSizes = [0, 24, 28, 32, 36, 40, 48, 56, 64];
+const fontSizes = [0, 36, 40, 48, 56, 64];//  TODO,需要查看设计稿
 const widths = [44, 240, 400, 480];
 const heights = [200, 300];
-const boxes = [80, 100, 120, 200];
+const boxes = [80, 100, 120, 200]; // 正方形盒子，不建议使用，盒子的大小，尤其是高度，应当由内容撑起
 module.exports = {
   // production下的tree shake
   purge: ["./src/**/*.html", "./src/**/*.js"],
@@ -68,33 +61,86 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    // extend: {},
-    colors: {
-      // 基本填充颜色列表 //浅黄#
-      "primary": "#5965ba",
-      "secondary": "#999",
-      "success": "#1bb73a",
-      "danger": "#e83930",
-      "warning": "#fa8c16",
-      "info": "#FFE56C",
-      "dark": "#333",
-      // 选中或者focus、hover等,暂缺
-      "primary-active": "#5965ba",
-      "secondary-active": "#999",
-      "success-active": "#1bb73a",
-      "danger-active": "#e83930",
-      "warning-active": "#fa8c16",
-      "info-active": "#FFE56C",
-      "dark-active": "#333",
-      // 不可操作,需要透明度,暂缺
-      "primary-disable": "#5965ba",
-      "secondary-disable": "#999",
-      "success-disable": "#1bb73a",
-      "danger-disable": "#e83930",
-      "warning-disable": "#fa8c16",
-      "info-disable": "#FFE56C",
-      "dark-disable": "#333",
+    extend: {
+      // 为了复用colors
+      textColor: {
+        // 基本文字颜色列表 文字的secondary和color的不一致，需要确认设计稿，缺失选中和disable
+        "basic": {
+          DEFAULT: "#333333",
+          active: "#333333",
+          disable: '"#333333"'
+        },
+        "muted": {
+          DEFAULT: "#999999",
+          active: "#999999",
+          disable: '"#999999"'
+        },
+        "light": {
+          DEFAULT: "#fff",
+          active: "#fff",
+          disable: '"#fff"'
+        },
+        "tinge": {
+          DEFAULT: "#b476c0",
+          active: "#b476c0",
+          disable: '"#b476c0"'
+        },
+        "secondary": {
+          DEFAULT: "#666666",
+          active: "#666666",
+          disable: '"#666666"'
+        },
+        "dark": {
+          DEFAULT: "#000",
+          active: "#000",
+          disable: '"#000"'
+        },
+        // "primary": "#5965ba",
+        // "success": "#1bb73a",
+        // "danger": "#e83930",
+        // "warning": "#fa8c16", //#FB9716
+        // "info": "#ffe56c",
+      },
     },
+    colors: {
+      // 基本填充颜色列表,缺失选中/禁用的效果
+      "primary": {
+        DEFAULT: "#5965ba",
+        active: "#5965ba",
+        disable: "#5965ba",
+      },
+      "secondary": {
+        DEFAULT: "#999",
+        active: "#999",
+        disable: "#999",
+      },
+      "success": {
+        DEFAULT: "#1bb73a",
+        active: "#1bb73a",
+        disable: "#1bb73a",
+      },
+      "danger": {
+        DEFAULT: "#e83930",
+        active: "#e83930",
+        disable: "#e83930",
+      },
+      "warning": {
+        DEFAULT: "#fa8c16",
+        active: "#fa8c16",
+        disable: "#fa8c16",
+      },
+      "info": {
+        DEFAULT: "#FFE56C",
+        active: "#FFE56C",
+        disable: "#FFE56C",
+      },
+      "dark": {
+        DEFAULT: "#333",
+        active: "#333",
+        disable: "#333",
+      },
+    },
+
     spacing: {
       px: '1px',
       ...spacings.reduce((pre, cur) => { pre[cur] = cur + 'px'; return pre }, {})
@@ -104,11 +150,42 @@ module.exports = {
     },
     heights: {
       ...heights.reduce((pre, cur) => { pre[cur] = cur + 'px'; return pre }, {})
+    },
+    fontSize: {
+      //字号和行高，12号字体1行高
+      sm: ['24px', '24px'],//最小12，12一下不能保证
+      base: ['28px', '40px'],
+      lg: ['32px', '48px'], // 以上三种为项目中主要字号，不以数字命名，其他特殊字号以数字命名
+      ...fontSizes.reduce((pre, cur) => { pre[cur] = cur + 'px'; return pre }, {})
     }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents }) {
+      addBase({
+        html: {
+          fontSize: '10vw',
+          // 默认字体：iOS 9+ Safari开始支持 -apple-system 参数， Chrome 使用 BlinkMacSystemFont ，兼容 iOS ／ MacOS，9以前的用Helvetica
+          // Microsoft YaHei 兼容win系统
+          // 安卓无法预知发行厂商预制何种字体
+          // 除非引入字体文件（太大不推荐），否则无法保证字体生效
+          fontFamily: 'PingFangSC-Regular,"PingFang SC","Microsoft YaHei",sans-serif,-apple-system,BlinkMacSystemFont,"Helvetica Neue"'
+        }
+      })
+
+      // .box-80
+      const boxComponents = boxes.map(size => ({
+        ['.box-' + size]: {
+          width: size + 'px',
+          height: size + 'px'
+        }
+      }))
+
+      addComponents(boxComponents)
+    }),
+  ],
+
 };
 
